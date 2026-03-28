50df4e87f56983581638022b514fb245cf74fdfd
RizkiRdm <rizkiromdhoni28.com>
2026-03-28 21:44:29 +0700

    feat: Environment config loader & create readme.md

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from core.config import settings
+from loguru import logger
+
+
 def main():
-    print("Hello from hedgeai!")
+    logger.info("HedgeAI System Initializing...")
+    logger.info(
+        f"Environment Loaded. Capital: ${settings.TOTAL_CAPITAL} | Dry Run: {settings.DRY_RUN}"
+    )
+
+    # Placeholder untuk Task 1.4 & 1.5
+    print("HedgeAI Foundation Ready.")
 
 
 if __name__ == "__main__":
